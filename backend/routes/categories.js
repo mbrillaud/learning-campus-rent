7f70d73eac56883096e1c46e8727aa6281db0703
Mehdi Brillaud <mehdibrillaud@mbp-mehdi.lan>
2024-04-04 15:08:10 +0200

    Déplace la logique des catégories du routeurs vers un controller

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,42 +1,11 @@
 const express = require('express');
-const Category = require('../models/Category');
-
+const categoriesCtrl = require('../controllers/categories');
 const router = express.Router();
 
-
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const category = new Category({
-        ...req.body
-    });
-
-    category.save()
-        .then(() => res.status(200).json({message: 'Category saved'}))
-        .catch(error => res.status(400).json({error}))
-})
-
-router.get('/', (req, res, next) => {
-    Category.find()
-      .then(categories => res.status(200).json(categories))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-router.get('/:id', (req, res, next) => {
-    Category.findOne({_id: req.params.id})
-        .then(categories => res.status(200).json(categories))
-        .catch(error => res.status(404).json({ error }));
-});
-
-router.delete('/:id', (req, res, next) => {
-    Category.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Category deleted'}))
-      .catch(error => res.status(404).json({ error }));
-});
-
-router.put('/:id', (req, res, next) => {
-    Category.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Category updated'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+router.post('/', categoriesCtrl.addCategory)
+router.get('/', categoriesCtrl.getCategories);
+router.get('/:id', categoriesCtrl.getCategory);
+router.delete('/:id', categoriesCtrl.deleteCategory);
+router.put('/:id', categoriesCtrl.updateCategory);
 
 module.exports = router;
