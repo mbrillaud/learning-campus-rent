143f5ab3d8d3964012787a8f45fa2a4026ecb889
Mehdi Brillaud <mehdibrillaud@mbp-mehdi.lan>
2024-04-04 12:50:52 +0200

    Finalise l'initialisation du projet

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 
 const dbCredentials = require('../dbCredentials');
-const credentials = dbCredentials.dbCredentials;
 
-mongoose.connect(`mongodb+srv://${credentials.user}:${credentials.password}@cluster0.pkvplib.mongodb.net/`)
+mongoose.connect(`mongodb+srv://car_rent_admin:${dbCredentials.dbPassword}@cluster0.pkvplib.mongodb.net/`)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((error) => console.log('Connexion à MongoDB échouée : ', error));
 
