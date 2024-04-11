9de129c0e8f17cc9cf554dba4957dec3a432e23a
Mehdi Brillaud <mehdibrillaud@mbp-mehdi.lan>
2024-04-11 15:45:03 +0200

    Supprime le middleware auth du routeur category

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const auth = require('../middlewares/auth');
 const categoryCtrl = require('../controllers/category');
 const router = express.Router();
 
 
 router.post('/', categoryCtrl.addCategory)
-router.get('/', auth, categoryCtrl.getCategories);
+router.get('/', categoryCtrl.getCategories);
 router.get('/:id', categoryCtrl.getCategory);
 router.delete('/:id', categoryCtrl.deleteCategory);
 router.put('/:id', categoryCtrl.updateCategory);
