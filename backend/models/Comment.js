c5ffcb45f17b03a2e4ff55e1c429251b14e91c74
Mehdi Brillaud <mehdibrillaud@mbp-mehdi.lan>
2024-04-16 12:21:30 +0200

    Ajoute les relations

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
+const user = require('./User');
+const product = require('./Product');
+const Schema = mongoose.Schema;
 
-const commentSchema = mongoose.Schema({
-    productId: {type: String, required: true},
-    userId: {type: String, required: true},
+const commentSchema = new Schema({
+    productId: {
+        type: Schema.Types.ObjectId,
+        ref: product
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: user
+    },
     comment: {type: String, required: true},
     createdAt: {type: Date, required: true}
 });
--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
+const category = require('./Category');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
     label: {type: String, required: true},
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: category
+    },
     description: {type: String, required: true},
     imageUrl: {type: String, required: true},
     ownerId: {type: String, required: true},
-    price: {type: Number, required: true},
-    category: [{
-        type: Schema.Types.ObjectId,
-        ref: ''
-    }],
+    price: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('Product', productSchema);
