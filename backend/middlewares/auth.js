ab72bfbbc9dc59cc73b255b00914c549cea39fa7
Mehdi Brillaud <mehdibrillaud@mbp-mehdi.lan>
2024-04-17 11:13:17 +0200

    Supprime un console.log dans middlewares/auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        console.log('token : ', token);
         const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
         const userId = decodedToken.userId;
         req.auth = {
